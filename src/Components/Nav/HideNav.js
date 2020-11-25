e6c85a79cd1275246fa94f845bb0af201223a358
cloudlee <cloudlee@MacBook-Pro-9.local>
2020-11-25 23:04:05 +0900

    Add: MainComponents Add

--- src/Components/Nav/HideNav.js
+++ src/Components/Nav/HideNav.js
@@ -2,21 +2,52 @@ import React from "react";
 import styled from "styled-components";
 
 export default function HideNav(props) {
-  const { navData, navKeys } = props;
+  const { navData, navKeys, hideNavData, eachKey, toggle } = props;
 
   return (
-    <HideNavContainer>
-      <div>
-        {navKeys.map((el) => (
-          <li>{el}</li>
+    <HideNavContainer toggle={toggle}>
+      <HideNavTitle>{eachKey}</HideNavTitle>
+      <KeyWordWrapper>
+        {hideNavData?.map((el, index) => (
+          <li key={hideNavData[index]}>{el}</li>
         ))}
-      </div>
+      </KeyWordWrapper>
     </HideNavContainer>
   );
 }
 
 const HideNavContainer = styled.section`
-  width: 100vw;
-  height: 10vh;
+  position: absolute;
+
+  display: ${(props) => (props.toggle ? "static" : "none")};
+  width: 100%;
   z-index: 100;
+  background-color: white;
+  ul {
+    li {
+      text-align: center;
+    }
+  }
+`;
+
+const HideNavTitle = styled.span`
+  width: 100%;
+  text-align: center;
+  display: block;
+  border: 1px solid gray;
+  border-radius: 3px;
+`;
+
+const KeyWordWrapper = styled.ul`
+  li {
+    text-align: center;
+    margin: 0.5vh 0;
+    transition: all 0.5s ease-in-out;
+    border: 1px solid white;
+
+    &:hover {
+      border: 1px solid gray;
+      border-radius: 3px;
+    }
+  }
 `;
--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -4,7 +4,32 @@ import styled from "styled-components";
 export default function Main() {
   return (
     <MainContainer>
-      <PictureCheck />
+      <BigContents>
+        <img
+          alt="bigPhoto"
+          src="https://cdn.shopify.com/s/files/1/0305/5853/files/modern_classic_1024x1024.jpg?v=1538508496"
+        />
+      </BigContents>
+      <SmallContentsWrapper>
+        <SmallContents>
+          <img
+            alt="smallPicture"
+            src="https://cdn.shopify.com/s/files/1/0305/5853/files/modern_classic_1024x1024.jpg?v=1538508496"
+          />
+          <span>Title</span>
+          <span>specific data</span>
+          <span>contents</span>
+        </SmallContents>
+        <SmallContents>
+          <img
+            alt="smallPicture"
+            src="https://cdn.shopify.com/s/files/1/0305/5853/files/modern_classic_1024x1024.jpg?v=1538508496"
+          />
+          <span>Title</span>
+          <span>specific data</span>
+          <span>contents</span>
+        </SmallContents>
+      </SmallContentsWrapper>
     </MainContainer>
   );
 }
@@ -16,6 +41,35 @@ const MainContainer = styled.main`
   display: flex;
   justify-content: center;
   background-color: white;
+`;
+
+const BigContents = styled.div`
+  width: 30vw;
+  height: 80vh;
+  margin: 2vh;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const SmallContentsWrapper = styled.section`
+  display: flex;
+  /* flex-direction: column; */
+  /* justify-content: space-evenly; */
+  align-items: center;
+  /* width: 30vw;
+  height: 40vh; */
+`;
+
+const SmallContents = styled.div`
+  width: 30vw;
+  height: 40vh;
+  margin: 2vh;
+  img {
+    width: 100%;
+    height: 100%;
+  }
 `;
 const PictureCheck = styled.div`
   background-image: url("https://cdn.shopify.com/s/files/1/0305/5853/files/modern_classic_1024x1024.jpg?v=1538508496");
